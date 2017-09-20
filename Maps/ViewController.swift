5a9d6cecb526642a7b5c9a28be33f7402c44e172
MohammadrezaAflaki <am823617@gmail.com>
2017-09-20 22:35:38 +0430

    Maps created

---
+++ Maps/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by mohammad 141 on 9/13/17.
+//  Copyright © 2017 mohammad 141. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    //Outlets
+    @IBOutlet weak var map: MKMapView!
+    
+
+    //Variables
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let latitude: CLLocationDegrees = 37.555
+        let longitude: CLLocationDegrees = 45.102778
+
+        let latDelta: CLLocationDegrees = 0.05
+
+        let lonDelta: CLLocationDegrees = 0.05
+
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        
+
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "My House"
+        
+        annotation.subtitle = "hehehhe"
+        
+        annotation.coordinate = location
+        
+        map.addAnnotation(annotation)
+        
+        
+        let longPress = UITapGestureRecognizer(target: self, action: #selector(self.longPress(gestureRecognizer:)))
+        
+        
+       // longPress.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(longPress)
+        
+    }
+    
+    @objc func longPress(gestureRecognizer: UIGestureRecognizer) {
+        
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        annotation.title = "Customm"
+        annotation.subtitle = "New"
+        
+        map.addAnnotation(annotation)
+        
+        
+    }
+
+    
+
+
+}
+
